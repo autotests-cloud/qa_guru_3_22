7234ae5449bd4c3516e9af1326aa32fefc22f527
svasenkov <aanher@gmail.com>
2021-02-01 20:29:07 +0300

    added demoqa tests

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,33 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import drivers.CustomWebDriver;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+
+public class TestBase {
+    @BeforeAll
+    static void setup() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.browser = CustomWebDriver.class.getName();
+
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach(){
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        attachVideo();
+
+        closeWebDriver();
+    }
+
+}
---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,55 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class TextBoxTests extends TestBase {
+
+    @Test
+    void fillFormTest() {
+        step("Open Text Box form", () -> {
+            open("https://demoqa.com/text-box");
+            $(".main-header").shouldHave(text("Text Box"));
+        });
+
+        step("Fill Text Box form", () -> {
+            $("#userName").val("Alex");
+            $("#userEmail").val("Egorov@alex.com");
+            $("#currentAddress").val("Montenegro");
+            $("#permanentAddress").val("Street 1");
+            $("#submit").click();
+        });
+
+        step("Verify successful form submit", () -> {
+            $("#output").shouldHave(text("Name:Alex\n" +
+                    "Email:Egorov@alex.com\n" +
+                    "Current Address :Montenegro\n" +
+                    "Permananet Address :Street 1"));
+        });
+    }
+
+    @Test
+    void wrongEmailTest() {
+        step("Open Text Box form", () -> {
+            open("https://demoqa.com/text-box");
+            $(".main-header").shouldHave(text("Text Box"));
+        });
+
+        step("Fill Text Box form", () -> {
+            $("#userName").val("Alex");
+            $("#userEmail").val("Egorov");
+            $("#currentAddress").val("Montenegro");
+            $("#permanentAddress").val("Street 1");
+            $("#submit").click();
+        });
+
+        step("Verify unsuccessful form submit", () -> {
+            $("#userEmail").shouldHave(cssClass("field-error"));
+        });
+    }
+}
